961610b1d8d172d7f01854e86965567993878807
mindMaximal <vamper.2012@gmail.com>
2019-08-06 14:53:29 +0800

    main

--- bot.php
+++ bot.php
@@ -63,12 +63,12 @@
 
   //$response = $telegram->getUpdates();
 
-  $chid = $result['callback_query']['from']['id']; 
-  //['callback_query']['data'];
-  $telegram->sendMessage([
-    'chat_id' => $chid,
-    'text' => 'Here is the callback: ' . $result
-  ]);
+  // $chid = $result['callback_query']['from']['id']; 
+  // //['callback_query']['data'];
+  // $telegram->sendMessage([
+  //   'chat_id' => $chid,
+  //   'text' => 'Here is the callback: ' . $result
+  // ]);
   
   $text = $result["message"]["text"];
   $chat_id = $result["message"]["chat"]["id"]; 
