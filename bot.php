10d5ca10556099395223ba66fb879138d645edb5
mindMaximal <vamper.2012@gmail.com>
2019-08-06 16:08:02 +0800

    main

--- bot.php
+++ bot.php
@@ -73,16 +73,17 @@
   //   'chat_id' => $chid,
   //   'text' => 'Here is the callback: ' . $result
   // ]);
+   $callback = $result['callback_query'];
+   $callback_id = $result['from']['id'];
+   $telegram->sendMessage([
+    'chat_id' => $callback_id,
+    'text' => 'Here is the callback: ' . $callback
+   ]);
   
   $text = $result["message"]["text"];
   $chat_id = $result["message"]["chat"]["id"]; 
   $name = $result["message"]["from"]["username"];
   $keyboard = [["\xf0\x9f\x94\xa5 Цены"],["\xf0\x9f\x8e\x81 Акции"],["\xf0\x9f\x93\x86 Забронировать"],["\xf0\x9f\x93\x8c Как нас найти?"]]; 
-
-  $telegram->sendMessage([
-    'chat_id' => $chat_id,
-    'text' => 'Here is tp: ' . $result
-  ]);
 
   $inline_button1 = array("text"=>"Google url","url"=>"http://google.com");
   $inline_button2 = array("text"=>"work plz","callback_data"=>'Test WeeBHOOK');
